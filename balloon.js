14a5c8bfff17670c26096c76d1eb753c565dadff
Rachelle Herrera <rachelle.herera@gmail.com>
2015-07-09 00:51:02 -0700

    created a feature to eliminate letters

--- balloon.js
+++ balloon.js
@@ -1,26 +1,73 @@
-var letterArray= 'abcdefghijklmnopqrstuvwxyz'.split('');
+var setIntervalId = setInterval(function () 
+ {createLetter()}, 3000); 
 
-var randomLetter = letterArray[Math.floor(Math.random() * 26)];
+//each time the function is called a random letter will appear 
 
-$(document).keypress(function (e) {
- if (String.fromCharCode(e.which) === randomLetter) {
-  console.log('yay');
- }
-})
+var letterArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+function createLetter() {  
+  var randomLetter = letterArray[Math.floor(Math.random() * 26)];
+  randomLetter = randomLetter.toLowerCase();
+  var letterDiv = document.createElement('div');
+  letterDiv.innerHTML = randomLetter;
+  letterDiv.className = 'letter';
+  letterDiv.id = randomLetter;
+  letterDiv.style.bottom = 1000 - (Math.random() * 500) + "px";
+  letterDiv.style.top = Math.random() * 10 + "px";
+  letterDiv.style.left = Math.random() * 300 + "px";
+  container = document.getElementById('container');
+  container.appendChild(letterDiv);
+
+  
+$(document).keydown( function (event) { 
+    var keycode = String.fromCharCode(event.keyCode);
+    keycode = keycode.toLowerCase(); 
+    console.log(keycode + " KEYCODE HERE");
+    console.log(randomLetter);
+    if(keycode === randomLetter){
+      $("#" + keycode).hide(); 
+    console.log("keys are the same");
+    } else { 
+    console.log("keys are not the same");
+    }
+  });
+}
+  
 
 
 
-// create div from javascript
-// or jquery would be better
+
+
+
+
+
+
+
+//style 
+//counter
+//decrease counter
+// time limit
+
+
+//create two players
+
+
+
+
+
+
+//create a gameover function using the intervalId 
+//var gameOver = function() {clearInterval(intervalId); 
+//  alert("GameOver"); 
+
+
+
 // how to animatite div falling jquery
-// 
-// 
 // if letter you type === random letter
-// do somethign
+// do something
 // like this.hide()
 // append('.pick')
 // time limit
 // score board
 // reset 
 // players
-// layout
